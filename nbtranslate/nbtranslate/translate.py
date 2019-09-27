d3c52851133f132b7ac87d3a1c37637bf4fbb3bf
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2019-09-27 11:46:21 +0300

    Preserve cell structure
    Translate cell line-by-line and keep leading spaces to preserve structures like nested lists.

--- nbtranslate/nbtranslate/translate.py
+++ nbtranslate/nbtranslate/translate.py
@@ -15,9 +15,15 @@ class TranslatePreprocessor(Preprocessor):
         """ Translate all Markdown cells to Russian """
 
         if 'source' in cell and cell.cell_type == "markdown":
-            translation = self.translate_client.translate(cell.source, target_language='ru')
+            # Google Translate API does not preserve newline symbol and 
+            # leading spaces (useful to keep nested lists)
+            lines = cell.source.split('\n')
+            translated_lines = [' ' * (len(line) - len(line.lstrip(' '))) +
+                                self.translate_client.translate(line, target_language='ru')['translatedText']
+                                for line in lines]
+            translation = '\n'.join(translated_lines)
             # Google Translate adds a space between ] and ( and after some / in URLs
-            cell.source = translation['translatedText'].replace('] (', '](').replace('/ ', '/')
+            cell.source = translation.replace('] (', '](').replace('/ ', '/')
 
         return cell, resources
 
